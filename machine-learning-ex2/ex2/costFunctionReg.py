363101133b9f414035064af1e071b94c3782fbd7
juan190199 <juanmanueldasilvatome@gmail.com>
2022-06-10 18:08:08 +0200

    ~// Cost function logreg + regularization

--- machine-learning-ex2/ex2/costFunctionReg.py
+++ machine-learning-ex2/ex2/costFunctionReg.py
@@ -14,6 +14,17 @@ def cost_function_reg(theta, X, y, lmd):
     #                You should set cost and grad correctly.
     #
 
+    hypothesis = sigmoid(np.dot(X, theta))
+
+    reg_theta = theta[1:]
+
+    cost = np.sum(-y * np.log(hypothesis) - (1 - y) * np.log(1 - hypothesis)) / m \
+           + (lmd / (2 * m)) * np.sum(reg_theta * reg_theta)
+
+    normal_grad = (np.dot(X.T, hypothesis - y) / m).flatten()
+
+    grad[0] = normal_grad[0]
+    grad[1:] = normal_grad[1:] + reg_theta * (lmd / m)
 
     # ===========================================================
 

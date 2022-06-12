fcf619b7870528315896653803c4e2ad77d454d7
juan190199 <juanmanueldasilvatome@gmail.com>
2022-06-12 17:20:56 +0200

    ~// Prediction logreg

--- machine-learning-ex2/ex2/predict.py
+++ machine-learning-ex2/ex2/predict.py
@@ -14,6 +14,12 @@ def predict(theta, X):
     #                You should set p to a 1D-array of 0's and 1's
     #
 
+    p = sigmoid(np.dot(X, theta))
+    pos = np.where(p >= 0.5)
+    neg = np.where(p < 0.5)
+
+    p[pos] = 1
+    p[neg] = 1
 
     # ===========================================================
 
